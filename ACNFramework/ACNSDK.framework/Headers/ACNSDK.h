66088f00adb9ce5d84c8bbb5fde78883df444247
superChan <chenchao@tataufo.com>
2020-03-11 13:25:07 +0800

    pay

---
+++ ACNFramework/ACNSDK.framework/Headers/ACNSDK.h
@@ -0,0 +1,19 @@
+//
+//  ACNSDK.h
+//  ACNSDK
+//
+//  Created by zhangliang on 2018/7/18.
+//  Copyright © 2018 tataufo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACNSDK.
+FOUNDATION_EXPORT double ACNSDKVersionNumber;
+
+//! Project version string for ACNSDK.
+FOUNDATION_EXPORT const unsigned char ACNSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACNSDK/PublicHeader.h>
+
+
